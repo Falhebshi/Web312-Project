fd36366e2e0599ca64e7814a71d1966ca3b7d005
hayahamad2 <hhaayyaa1467@gmail.com>
2025-11-24 03:07:35 +0300

    feat: add form validation and submission handling for join us section

--- script.js
+++ script.js
@@ -90,3 +90,55 @@ function sortServices() {
    let value = select.value;
    let sortedCards = cards.slice();
 }
+
+const form = document.querySelector(".join-us");
+form.addEventListener("submit",function(e) {
+  e.preventDefault();
+
+    const nameField = document.getElementById("member-name");
+    const emailField = document.getElementById("member-email");
+    const dobField = document.getElementById("date-birth");
+    const experField = document.getElementById("experties-member");
+    const skillsField = document.getElementById("skills-member");
+    const eduField = document.getElementById("education-member");
+    const msgField = document.getElementById("message-member");
+    const photoField = document.getElementById("service-photo");
+  //no empty field
+  if (
+        nameField.value.trim() === "" ||
+        emailField.value.trim() === "" ||
+        dobField.value === "" ||
+        experField.value.trim() === "" ||
+        skillsField.value.trim() === "" ||
+        eduField.value.trim() === "" ||
+        msgField.value.trim() === "" ||
+        !photoField.files.length
+    ) {
+        alert("الرجاء تعبة جميع الحقول!");
+        return;
+    }
+    //name field does't start with numbers
+    if (/^[0-9]/.test(nameField.value.trim())) {
+        alert("الاسم لا يمكن أن يبدأ برقم!");
+        return;
+    }
+
+    //Photo field accepts only image
+    const photo = photoField.files[0];
+     if (photo && !photo.type.startsWith("image/")) {
+        alert("الرجاء رفع صورة فقط!");
+        return;
+    }
+
+    //DOB should not be after 2008
+    const maxDate = new Date("2008-12-31");
+    const userDate = new Date(dobField.value);
+
+    if (userDate > maxDate) {
+        alert("تاريخ الميلاد يجب أن يكون قبل عام 2008! ");
+        return;
+    }
+    alert("تم إرسال الطلب بنجاح ! شكراً لك يا "+ nameField.value);
+    form.submit();
+
+});
