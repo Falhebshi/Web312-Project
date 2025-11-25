7990fb4c2bc27fa745a11ba2473547cda8d4842f
hayahamad2 <hhaayyaa1467@gmail.com>
2025-11-25 12:15:05 +0300

    add top-up button, and modify footer to Lama code for home page and about-us and staff pages

--- script.js
+++ script.js
@@ -200,3 +200,21 @@ setInterval(updateClock, 1000);
 // تشغيلها بمجرد تحميل الصفحة
 updateClock();
 
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
